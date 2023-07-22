4ecad733b6a035ae573e72a85f9fc6bfe5c6cb2b
Ashistorq <ashishsinghtarwa@gmail.com>
2023-07-22 11:40:38 +0530

    reponse time set

--- Project 1/index.js
+++ Project 1/index.js
@@ -5,25 +5,23 @@ const app = express();
 
 app.get("/numbers", async (req, res) => {
   const urls = req.query.url;
-
-  if (!urls || !Array.isArray(urls)) {
-    return res.status(400).json({ error: "Invalid URL list" });
-  }
+  console.log(urls);
+  // if (!urls || !Array.isArray(urls)) {
+  //   return res.status(400).json({ error: "Invalid URL list" });
+  // }
 
   try {
-    const responses = await Promise.all(
-      urls.map((url) => axios.get(url).then((response) => response.data))
-    );
+    const response = await fetch(urls);
+    const data = await response.json();
+    console.log(data);
+    const mergedData = data.numbers.flat().sort((a, b) => a - b);
 
-    // Process the responses if needed and return the combined data
-    // (Here, we are just returning the array of responses as is)
-    res.json(responses);
+    return res.json({ data: mergedData });
   } catch (error) {
-    res.status(500).json({ error: "Error fetching data from URLs" });
+    return res.status(500).json({ error: error.message });
   }
 });
 
 app.listen(8008, () => {
-  console.log(`Server is running on http://localhost:8008`);
+  console.log("Server is running on port 8008");
 });
-
